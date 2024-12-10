9a254ea5b7bdbef8086595e002b5ed23b4b4c058
Walter Vos <walter.vos@ns.nl>
2024-12-10 13:23:25 +0100

    Logging

--- Day09.fs
+++ Day09.fs
@@ -8,12 +8,13 @@ type Block =
     override this.ToString() : string =
         match this with
         | FileBlock id -> $"{id}"
-        | FreeBlock length -> [ for i in 1..length -> '.'] |> List.toArray |> System.String
+        | FreeBlock length ->
+            [ for i in 1..length -> '.' ] |> List.toArray |> System.String
 
 
 module Day09 =
     let day = 9
-    
+
     let isFree =
         function
         | FreeBlock _ -> true
@@ -21,22 +22,29 @@ module Day09 =
 
     let isFile = isFree >> not
 
-    let rec move blocks = // Nope. You need as many spaces as the file ID is long to move a file there
+    let rec move blocks = // Nope. You need as many spaces as the file ID is long to move a file there. Or do you?
         match
             blocks |> Seq.tryFindIndex isFree,
             blocks |> Seq.tryFindIndexBack isFile
         with
-        | Some firstFreeBlock, Some lastFileBlock when lastFileBlock >= firstFreeBlock ->
+        | Some firstFreeBlock, Some lastFileBlock when
+            lastFileBlock >= firstFreeBlock
+            ->
+            printfn
+                "Swapped free block at index %i with file block at index %i (%.2f%%)"
+                firstFreeBlock
+                lastFileBlock
+                ((firstFreeBlock |> double) / (lastFileBlock |> double) * 100.)
+
             blocks |> Seq.swap firstFreeBlock lastFileBlock |> move
         | _ -> blocks
 
     let checksum blocks =
         blocks
-        |> Seq.mapi (fun position block -> 
+        |> Seq.mapi (fun position block ->
             match block with
             | FileBlock id -> position * id
-            | _ -> 0
-        )
+            | _ -> 0)
         |> Seq.map int64
         |> Seq.sum
 
@@ -46,12 +54,14 @@ module Day09 =
         |> Seq.mapi (fun index size ->
             seq {
                 for _ in 0 .. size - 1 do
-                    if index % 2 = 0 then FileBlock(index / 2) else FreeBlock size
+                    if index % 2 = 0 then
+                        FileBlock(index / 2)
+                    else
+                        FreeBlock size
             })
         |> Seq.concat
 
-    let solve puzzle =
-        puzzle |> parse |> move |> checksum
+    let solve puzzle = puzzle |> parse |> move |> checksum
 
     let main =
         (fun () ->
