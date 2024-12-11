b199caaa47b109c1e543e0cc3ef9248c9e5181b0
Walter Vos <walter.vos@ns.nl>
2024-12-11 10:35:41 +0100

    Day 9 part 2, big fun!

--- Day09.fs
+++ Day09.fs
@@ -10,10 +10,9 @@ type Block =
         | FileBlock id -> $"{id}"
         | FreeBlock -> "."
 
-
 module Day09 =
     let day = 9
-    
+
     let isFree =
         function
         | FreeBlock -> true
@@ -21,31 +20,97 @@ module Day09 =
 
     let isFile = isFree >> not
 
-    let rec move blocks =
-        match
-            blocks |> Array.tryFindIndex isFree,
-            blocks |> Array.tryFindIndexBack isFile
-        with
-        | Some firstFreeBlockIndex, Some lastFileBlockIndex when lastFileBlockIndex > firstFreeBlockIndex ->
-            
-            Array.set blocks firstFreeBlockIndex blocks[lastFileBlockIndex]
-            Array.set blocks lastFileBlockIndex FreeBlock
 
-            printfn "Swapped free block at position %i with last file block at position %i (%.2f%%)"
-                firstFreeBlockIndex
-                lastFileBlockIndex
-                ((firstFreeBlockIndex |> double) / (lastFileBlockIndex |> double) * 100.)
-            
-            blocks |> move
-        | _ -> blocks
+    let findLastFile' inBlocks =
+        match inBlocks |> Array.tryFindIndexBack isFile with
+        | Some index ->
+            inBlocks[0..index]
+            |> Array.indexed
+            |> Array.rev
+            |> Array.takeWhile (fun (i, b) -> isFile b)
+            |> (fun fileBlocks ->
+                match fileBlocks |> Array.head with
+                | _, (FileBlock _ as fb) ->
+                    fileBlocks |> Array.takeWhile (fun (i, b) -> b = fb)
+                | _ -> fileBlocks)
+            |> Array.rev
+            |> Array.map fst
+        | _ -> [||]
+
+    let findLastFile downFrom (inBlocks: Block array) =
+        inBlocks[0..downFrom] |> findLastFile'
+
+    let tryFindFreeBlocks ofSize (inBlocks: Block array) =
+        let inner ofSize inBlocks startingFrom =
+            inBlocks
+            |> Array.indexed
+            |> Array.skip startingFrom
+            |> Array.windowed ofSize
+            |> Array.tryFind (Array.forall (snd >> isFree))
+            |> (function
+            | Some indexedBlocks -> Some(indexedBlocks |> Array.map fst)
+            | _ -> None)
+
+        inner ofSize inBlocks 0
+
+    let compactFragmented blocks =
+        let rec move blocks =
+            match
+                blocks |> Array.tryFindIndex isFree,
+                blocks |> Array.tryFindIndexBack isFile
+            with
+            | Some firstFreeBlockIndex, Some lastFileBlockIndex when
+                lastFileBlockIndex > firstFreeBlockIndex
+                ->
+
+                Array.set blocks firstFreeBlockIndex blocks[lastFileBlockIndex]
+                Array.set blocks lastFileBlockIndex FreeBlock
+
+                printfn "Swapped free block at position %i with last file block at position %i (%.2f%%)"
+                    firstFreeBlockIndex
+                    lastFileBlockIndex
+                    ((firstFreeBlockIndex |> double) / (lastFileBlockIndex |> double) * 100.)
+
+                blocks |> move
+            | _ -> blocks
+
+        move blocks
+
+    let compactDefragmented blocks =
+        let rec move blocks maxIndexSeen =
+            let lastFileIndexes = findLastFile (maxIndexSeen - 1) blocks
+
+            if lastFileIndexes.Length = 0 then
+                blocks
+            else
+                match blocks |> tryFindFreeBlocks lastFileIndexes.Length with
+                | Some freeIndexes when
+                    (freeIndexes |> Array.last) < (lastFileIndexes |> Array.head)
+                    ->
+
+                    for (i, index) in lastFileIndexes |> Array.indexed do
+                        Array.set blocks freeIndexes[i] blocks[index]
+                        Array.set blocks index FreeBlock
+
+                    printfn
+                        "Swapped free block at position %i with last file block at position %A (%.2f%% to go)"
+                        (freeIndexes |> Array.head)
+                        (lastFileIndexes |> Array.head)
+                        ((lastFileIndexes |> Array.last |> double) / (double blocks.Length)
+                            * 100.)
+
+                    move blocks (lastFileIndexes |> Array.head)
+                | _ ->
+                    move blocks (lastFileIndexes |> Array.head)
+
+        move blocks blocks.Length
 
     let checksum blocks =
         blocks
-        |> Array.mapi (fun position block -> 
+        |> Array.mapi (fun position block ->
             match block with
             | FileBlock id -> uint64 (position * id)
-            | _ -> 0UL
-        )
+            | _ -> 0UL)
         |> Array.sum
 
     let parse puzzle =
@@ -53,17 +118,16 @@ module Day09 =
         |> Seq.map (string >> int)
         |> Seq.toArray
         |> Array.mapi (fun index size ->
-            [|
-                for _ in 0 .. size - 1 do
-                    if index % 2 = 0 then FileBlock(index / 2) else FreeBlock
-            |])
+            [| for _ in 0 .. size - 1 do
+                   if index % 2 = 0 then FileBlock(index / 2) else FreeBlock |])
         |> Array.concat
 
     let solve puzzle =
-        puzzle |> parse |> move |> checksum
+        puzzle |> parse |> compactFragmented |> checksum,
+        puzzle |> parse |> compactDefragmented |> checksum
 
     let main =
         (fun () ->
-            let part1 = Library.getInputForDay day |> solve
+            let part1, part2 = Library.getInputForDay day |> solve
 
-            $"Solutions for day {day}:\nPart 1: {part1}\n")
+            $"Solutions for day {day}:\nPart 1: {part1}\nPart 2: {part2}\n")
--- Day09Tests.fs
+++ Day09Tests.fs
@@ -54,15 +54,143 @@ let ``Parsing a small example`` () =
 [<Test>]
 let ``It moves one file`` () =
     [| FileBlock 0; FreeBlock; FileBlock 1 |]
-    |> move
+    |> compactFragmented
     |> should equal [| FileBlock 0; FileBlock 1; FreeBlock |]
 
 [<Test>]
 let ``It moves files until done`` () =
-    diskBefore () |> move |> should equal diskAfter
+    diskBefore () |> compactFragmented |> should equal diskAfter
 
 [<Test>]
-let ``Solving the large example`` () =
+let ``Solving the large example for part 1 and 2`` () =
     let example = "2333133121414131402"
 
-    example |> solve |> should equal 1928
+    example |> solve |> should equal (1928, 2858)
+
+[<Test>]
+let ``Finding all the block indexes of the last file`` () =
+    [| FreeBlock; FileBlock 0; FileBlock 0 |]
+    |> findLastFile'
+    |> should equal [| 1; 2 |]
+
+[<Test>]
+let ``Finding all the block indexes of the last file, skipping indexes already visited``
+    ()
+    =
+    [| FileBlock 0
+       FreeBlock
+       FileBlock 1
+       FreeBlock
+       FileBlock 2
+       FileBlock 2 |]
+    |> findLastFile 3
+    |> should equal [| 2 |]
+
+[<Test>]
+let ``Finding enough free blocks to store a file`` () =
+    (2, [| FreeBlock; FreeBlock; FileBlock 0; FileBlock 0 |])
+    ||> tryFindFreeBlocks
+    |> should equal (Some [| 0; 1 |])
+
+[<Test>]
+let ``Finding insufficient free blocks to store a file`` () =
+    (2, [| FreeBlock; FileBlock 0; FileBlock 0 |])
+    ||> tryFindFreeBlocks
+    |> should equal None
+
+
+[<Test>]
+let ``Moving a complete file, part 2`` () =
+    [| FreeBlock; FreeBlock; FileBlock 0; FileBlock 0 |]
+    |> compactDefragmented
+    |> should equal [| FileBlock 0; FileBlock 0; FreeBlock; FreeBlock |]
+
+[<Test>]
+let ``Moving a complete file when there's no space, part 2`` () =
+    [| FreeBlock; FileBlock 0; FileBlock 0 |]
+    |> compactDefragmented
+    |> should equal [| FreeBlock; FileBlock 0; FileBlock 0 |]
+
+[<Test>]
+let ``Moving a complete file when there's nothing to move, part 2`` () =
+    [| FileBlock 0; FileBlock 0; FreeBlock; FreeBlock |]
+    |> compactDefragmented
+    |> should equal [| FileBlock 0; FileBlock 0; FreeBlock; FreeBlock |]
+
+[<Test>]
+let ``It only moves the second last file when the last file is too big`` () =
+    [| FreeBlock
+       FreeBlock
+       FileBlock 0
+       FreeBlock
+       FileBlock 1
+       FileBlock 1
+       FileBlock 1 |]
+    |> compactDefragmented
+    |> should
+        equal
+        [| FileBlock 0
+           FreeBlock
+           FreeBlock
+           FreeBlock
+           FileBlock 1
+           FileBlock 1
+           FileBlock 1 |]
+
+[<Test>]
+let ``It compacts the example disc in part 2`` () =
+    let example = "2333133121414131402"
+
+    example
+    |> parse
+    |> compactDefragmented
+    |> should
+        equal
+        [| FileBlock 0
+           FileBlock 0
+           FileBlock 9
+           FileBlock 9
+           FileBlock 2
+           FileBlock 1
+           FileBlock 1
+           FileBlock 1
+           FileBlock 7
+           FileBlock 7
+           FileBlock 7
+           FreeBlock
+           FileBlock 4
+           FileBlock 4
+           FreeBlock
+           FileBlock 3
+           FileBlock 3
+           FileBlock 3
+           FreeBlock
+           FreeBlock
+           FreeBlock
+           FreeBlock
+           FileBlock 5
+           FileBlock 5
+           FileBlock 5
+           FileBlock 5
+           FreeBlock
+           FileBlock 6
+           FileBlock 6
+           FileBlock 6
+           FileBlock 6
+           FreeBlock
+           FreeBlock
+           FreeBlock
+           FreeBlock
+           FreeBlock
+           FileBlock 8
+           FileBlock 8
+           FileBlock 8
+           FileBlock 8
+           FreeBlock
+           FreeBlock |]
+
+[<Test>]
+let ``It keeps searching until it finds a suitable free block`` () =
+    [| FileBlock 0; FreeBlock; FileBlock 1; FileBlock 1; FreeBlock; FreeBlock; FileBlock 2 |]
+    |> tryFindFreeBlocks 2
+    |> should equal (Some [|4;5|])
